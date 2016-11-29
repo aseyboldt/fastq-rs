f2fb6235fd08ca577058ab0912258dc72f97bc28
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2016-11-29 19:41:56 +0100

    Add seqan example

---
+++ examples/readfq.cpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <iostream>
+
+#include <seqan/seq_io.h>
+
+using namespace seqan;
+
+int main(int argc, char const ** argv)
+{
+    if (argc != 2)
+        return 1;
+
+    StringSet<CharString> ids;
+    StringSet<CharString> seqs;
+
+    SeqFileIn reader(argv[1]);
+
+    while (!atEnd(reader))
+    {
+        readRecords(ids, seqs, reader, 50);
+    }
+
+    return 0;
+}
