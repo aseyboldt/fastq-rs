efb4ae69e7aae0fed8f1541fb38ae1605bf3b1d0
Seth Stadick <sstadick@gmail.com>
2020-03-16 08:43:22 -0700

    split record type

--- src/records.rs
+++ src/records.rs
@@ -1,15 +1,19 @@
-use std::io::{Write, Error, Result, ErrorKind};
 use memchr::memchr;
+use std::io::{Error, ErrorKind, Result, Write};
 
+pub enum RecordTypes {
+    Fastq,
+    Fasta,
+}
 
 /// Trait to be implemented by types that represent fastq records.
 pub trait Record {
     /// Return the fastq sequence as byte slice
     fn seq(&self) -> &[u8];
-    /// Return the id-line of the record as byte slice
+
+    /// Return id line of the record as a byte slice
     fn head(&self) -> &[u8];
-    /// Return the quality of the bases as byte slice
-    fn qual(&self) -> &[u8];
+
     /// Write the record to a writer
     fn write<W: Write>(&self, writer: &mut W) -> Result<usize>;
 
@@ -18,7 +22,9 @@ pub trait Record {
     /// FIXME This might be much faster with a [bool; 256] array
     /// or using some simd instructions (eg with the jetscii crate).
     fn validate_dna(&self) -> bool {
-        self.seq().iter().all(|&x| x == b'A' || x == b'C' || x == b'T' || x == b'G')
+        self.seq()
+            .iter()
+            .all(|&x| x == b'A' || x == b'C' || x == b'T' || x == b'G')
     }
 
     /// Return true if the sequence contains only A, C, T, G and N.
@@ -26,14 +32,38 @@ pub trait Record {
     /// FIXME This might be much faster with a [bool; 256] array
     /// or using some simd instructions (eg with the jetscii crate).
     fn validate_dnan(&self) -> bool {
-        self.seq().iter().all(|&x| x == b'A' || x == b'C' || x == b'T' || x == b'G' || x == b'N')
+        self.seq()
+            .iter()
+            .all(|&x| x == b'A' || x == b'C' || x == b'T' || x == b'G' || x == b'N')
     }
 }
 
+/// A fasta record that borrows data from an array.
+#[derive(Debug)]
+pub struct RefRecordFa<'a> {
+    // (start, stop), but might include \r at the end
+    head: usize,
+    seq: usize,
+    data: &'a [u8],
+}
+
+/// A fasta record that ownes its data arrays.
+#[derive(Debug)]
+pub struct OwnedRecordFa {
+    pub head: Vec<u8>,
+    pub seq: Vec<u8>,
+}
+
+#[derive(Debug)]
+pub struct IdxRecordFa {
+    head: usize,
+    seq: usize,
+    pub data: (usize, usize),
+}
 
 /// A fastq record that borrows data from an array.
 #[derive(Debug)]
-pub struct RefRecord<'a> {
+pub struct RefRecordFq<'a> {
     // (start, stop), but might include \r at the end
     head: usize,
     seq: usize,
@@ -44,7 +74,7 @@ pub struct RefRecord<'a> {
 
 /// A fastq record that ownes its data arrays.
 #[derive(Debug)]
-pub struct OwnedRecord {
+pub struct OwnedRecordFq {
     pub head: Vec<u8>,
     pub seq: Vec<u8>,
     pub sep: Option<Vec<u8>>,
@@ -52,7 +82,7 @@ pub struct OwnedRecord {
 }
 
 #[derive(Debug)]
-pub struct IdxRecord {
+pub struct IdxRecordFq {
     head: usize,
     seq: usize,
     sep: usize,
@@ -60,7 +90,7 @@ pub struct IdxRecord {
     pub data: (usize, usize),
 }
 
-
+// TODO: Why is this even needed?
 /// Remove a final '\r' from a byte slice
 #[inline]
 fn trim_winline(line: &[u8]) -> &[u8] {
@@ -71,22 +101,23 @@ fn trim_winline(line: &[u8]) -> &[u8] {
     }
 }
 
+impl<'a> RefRecordFq<'a> {
+    #[inline]
+    fn qual(&self) -> &[u8] {
+        trim_winline(&self.data[self.sep + 1..self.qual])
+    }
+}
 
-impl<'a> Record for RefRecord<'a> {
+impl<'a> Record for RefRecordFq<'a> {
     #[inline]
     fn head(&self) -> &[u8] {
         // skip the '@' at the beginning
-        trim_winline(&self.data[1 .. self.head])
+        trim_winline(&self.data[1..self.head])
     }
 
     #[inline]
     fn seq(&self) -> &[u8] {
-        trim_winline(&self.data[self.head + 1 .. self.seq])
-    }
-
-    #[inline]
-    fn qual(&self) -> &[u8] {
-        trim_winline(&self.data[self.sep + 1 .. self.qual])
+        trim_winline(&self.data[self.head + 1..self.seq])
     }
 
     #[inline]
@@ -96,8 +127,13 @@ impl<'a> Record for RefRecord<'a> {
     }
 }
 
+impl OwnedRecordFq {
+    fn qual(&self) -> &[u8] {
+        &self.qual
+    }
+}
 
-impl Record for OwnedRecord {
+impl Record for OwnedRecordFq {
     fn head(&self) -> &[u8] {
         // skip the '@' at the beginning
         &self.head
@@ -105,10 +141,6 @@ impl Record for OwnedRecord {
 
     fn seq(&self) -> &[u8] {
         &self.seq
-    }
-
-    fn qual(&self) -> &[u8] {
-        &self.qual
     }
 
     fn write<W: Write>(&self, writer: &mut W) -> Result<usize> {
@@ -119,8 +151,8 @@ impl Record for OwnedRecord {
         written += writer.write(self.seq())?;
         written += writer.write(b"\n")?;
         match self.sep {
-            Some(ref s) => { written += writer.write(s)? }
-            None => { written += writer.write(b"+")? }
+            Some(ref s) => written += writer.write(s)?,
+            None => written += writer.write(b"+")?,
         }
         written += writer.write(b"\n")?;
         written += writer.write(self.qual())?;
@@ -129,57 +161,137 @@ impl Record for OwnedRecord {
     }
 }
 
+impl<'a> Record for RefRecordFa<'a> {
+    #[inline]
+    fn head(&self) -> &[u8] {
+        // skip the '@' at the beginning
+        trim_winline(&self.data[1..self.head])
+    }
 
-pub enum IdxRecordResult {
+    #[inline]
+    fn seq(&self) -> &[u8] {
+        trim_winline(&self.data[self.head + 1..self.seq])
+    }
+
+    #[inline]
+    fn write<W: Write>(&self, writer: &mut W) -> Result<usize> {
+        writer.write_all(&self.data)?;
+        Ok(self.data.len())
+    }
+}
+
+impl Record for OwnedRecordFa {
+    fn head(&self) -> &[u8] {
+        // skip the '@' at the beginning
+        &self.head
+    }
+
+    fn seq(&self) -> &[u8] {
+        &self.seq
+    }
+
+    fn write<W: Write>(&self, writer: &mut W) -> Result<usize> {
+        let mut written = 0;
+        written += writer.write(b">")?;
+        written += writer.write(self.head())?;
+        written += writer.write(b"\n")?;
+        written += writer.write(self.seq())?;
+        written += writer.write(b"\n")?;
+        Ok(written)
+    }
+}
+
+pub enum IdxRecordFaResult {
     Incomplete,
     EmptyBuffer,
-    Record(IdxRecord),
+    Record(IdxRecordFa),
 }
 
+pub enum IdxRecordFqResult {
+    Incomplete,
+    EmptyBuffer,
+    Record(IdxRecordFq),
+}
 
-#[inline]
-fn read_header(buffer: &[u8]) -> Result<Option<usize>> {
-    match buffer.first() {
-        None => { Ok(None) },
-        Some(&b'@') => {
-            Ok(memchr(b'\n', buffer))
-        },
-        Some(_) => {
-            return Err(Error::new(ErrorKind::InvalidData,
-                                  "Fastq headers must start with '@'"))
+trait RecordReader {
+    fn read_header(buffer: &[u8]) -> Result<Option<usize>>;
+}
+struct FastqReader {}
+struct FastaReader {}
+
+impl RecordReader for FastaReader {
+    #[inline]
+    fn read_header(buffer: &[u8]) -> Result<Option<usize>> {
+        match buffer.first() {
+            None => Ok(None),
+            Some(&b'>') => Ok(memchr(b'\n', buffer)),
+            Some(_) => {
+                return Err(Error::new(
+                    ErrorKind::InvalidData,
+                    "Fasta headers must start with '>'",
+                ))
+            }
         }
     }
 }
 
-
-#[inline]
-fn read_sep(buffer: &[u8]) -> Result<Option<usize>> {
-    match buffer.first() {
-        None => { return Ok(None) },
-        Some(&b'+') => { Ok(memchr(b'\n', buffer)) },
-        Some(_) => {
-            return Err(Error::new(ErrorKind::InvalidData,
-                                  "Sequence and quality not separated by +"));
+impl RecordReader for FastqReader {
+    #[inline]
+    fn read_header(buffer: &[u8]) -> Result<Option<usize>> {
+        match buffer.first() {
+            None => Ok(None),
+            Some(&b'@') => Ok(memchr(b'\n', buffer)),
+            Some(_) => {
+                return Err(Error::new(
+                    ErrorKind::InvalidData,
+                    "Fastq headers must start with '@'",
+                ))
+            }
         }
     }
 }
 
+impl FastqReader {
+    #[inline]
+    fn read_sep(buffer: &[u8]) -> Result<Option<usize>> {
+        match buffer.first() {
+            None => return Ok(None),
+            Some(&b'+') => Ok(memchr(b'\n', buffer)),
+            Some(_) => {
+                return Err(Error::new(
+                    ErrorKind::InvalidData,
+                    "Sequence and quality not separated by +",
+                ));
+            }
+        }
+    }
+}
 
-impl<'a> RefRecord<'a> {
+impl<'a> RefRecordFq<'a> {
     /// Copy the borrowed data array and return an owned record.
-    pub fn to_owned_record(&self) -> OwnedRecord {
-        OwnedRecord {
+    pub fn to_owned_record(&self) -> OwnedRecordFq {
+        OwnedRecordFq {
             seq: self.seq().to_vec(),
             qual: self.qual().to_vec(),
             head: self.head().to_vec(),
-            sep: Some(trim_winline(&self.data[self.seq + 1..self.sep]).to_vec())
+            sep: Some(trim_winline(&self.data[self.seq + 1..self.sep]).to_vec()),
         }
     }
 }
 
-impl IdxRecord {
+impl<'a> RefRecordFa<'a> {
+    /// Copy the borrowed data array and return an owned record.
+    pub fn to_owned_record(&self) -> OwnedRecordFa {
+        OwnedRecordFa {
+            seq: self.seq().to_vec(),
+            head: self.head().to_vec(),
+        }
+    }
+}
+
+impl IdxRecordFq {
     #[inline]
-    pub fn to_ref_record<'a>(&self, buffer: &'a [u8]) -> RefRecord<'a> {
+    pub fn to_ref_record<'a>(&self, buffer: &'a [u8]) -> RefRecordFq<'a> {
         let data = &buffer[self.data.0..self.data.1];
         let datalen = data.len();
         debug_assert!(datalen == self.data.1 - self.data.0);
@@ -192,7 +304,7 @@ impl IdxRecord {
         debug_assert!(self.seq < self.sep);
         debug_assert!(self.sep < self.qual);
 
-        RefRecord {
+        RefRecordFq {
             data: data,
             head: self.head,
             seq: self.seq,
@@ -202,51 +314,94 @@ impl IdxRecord {
     }
 
     #[inline]
-    pub fn from_buffer(buffer: &[u8]) -> Result<IdxRecordResult> {
+    pub fn from_buffer(buffer: &[u8]) -> Result<IdxRecordFqResult> {
         if buffer.len() == 0 {
-            return Ok(IdxRecordResult::EmptyBuffer);
+            return Ok(IdxRecordFqResult::EmptyBuffer);
         }
 
-
-        let head_end = match read_header(buffer)? {
-            None => { return Ok(IdxRecordResult::Incomplete) },
-            Some(val) => val
+        let head_end = match FastqReader::read_header(buffer)? {
+            None => return Ok(IdxRecordFqResult::Incomplete),
+            Some(val) => val,
         };
         let pos = head_end + 1;
 
         let buffer_ = &buffer[pos..];
         let seq_end = match memchr(b'\n', buffer_) {
-            None => { return Ok(IdxRecordResult::Incomplete) },
-            Some(end) => end + pos
+            None => return Ok(IdxRecordFqResult::Incomplete),
+            Some(end) => end + pos,
         };
         let pos = seq_end + 1;
 
         let buffer_ = &buffer[pos..];
-        let sep_end = match read_sep(buffer_)? {
-            None => { return Ok(IdxRecordResult::Incomplete) },
+        let sep_end = match FastqReader::read_sep(buffer_)? {
+            None => return Ok(IdxRecordFqResult::Incomplete),
             Some(end) => end + pos,
         };
         let pos = sep_end + 1;
 
         let buffer_ = &buffer[pos..];
         let qual_end = match memchr(b'\n', buffer_) {
-            None => { return Ok(IdxRecordResult::Incomplete) },
+            None => return Ok(IdxRecordFqResult::Incomplete),
             Some(end) => end + pos,
         };
 
         if qual_end - sep_end != seq_end - head_end {
-            return Err(Error::new(ErrorKind::InvalidData,
-                                  "Sequence and quality length mismatch"));
+            return Err(Error::new(
+                ErrorKind::InvalidData,
+                "Sequence and quality length mismatch",
+            ));
         }
 
-        Ok(IdxRecordResult::Record(
-            IdxRecord {
-                data: (0, qual_end + 1),
-                head: head_end,
-                seq: seq_end,
-                sep: sep_end,
-                qual: qual_end,
-            }
-        ))
+        Ok(IdxRecordFqResult::Record(IdxRecordFq {
+            data: (0, qual_end + 1),
+            head: head_end,
+            seq: seq_end,
+            sep: sep_end,
+            qual: qual_end,
+        }))
+    }
+}
+
+impl IdxRecordFa {
+    #[inline]
+    pub fn to_ref_record<'a>(&self, buffer: &'a [u8]) -> RefRecordFa<'a> {
+        let data = &buffer[self.data.0..self.data.1];
+        let datalen = data.len();
+        debug_assert!(datalen == self.data.1 - self.data.0);
+
+        debug_assert!(self.head < datalen);
+        debug_assert!(self.seq < datalen);
+        debug_assert!(self.head < self.seq);
+
+        RefRecordFa {
+            data: data,
+            head: self.head,
+            seq: self.seq,
+        }
+    }
+
+    #[inline]
+    pub fn from_buffer(buffer: &[u8]) -> Result<IdxRecordFaResult> {
+        if buffer.len() == 0 {
+            return Ok(IdxRecordFaResult::EmptyBuffer);
+        }
+
+        let head_end = match FastaReader::read_header(buffer)? {
+            None => return Ok(IdxRecordFaResult::Incomplete),
+            Some(val) => val,
+        };
+        let pos = head_end + 1;
+
+        // TODO: handle multiline seqs
+        let buffer_ = &buffer[pos..];
+        let seq_end = match memchr(b'\n', buffer_) {
+            None => return Ok(IdxRecordFaResult::Incomplete),
+            Some(end) => end + pos,
+        };
+        Ok(IdxRecordFaResult::Record(IdxRecordFa {
+            data: (0, seq_end + 1),
+            head: head_end,
+            seq: seq_end,
+        }))
     }
 }
